3e7af3ef1ca703f38704b62aa2b43962de7b8e24
Paulo Neves <ptsneves@gmail.com>
2022-03-10 15:31:48 +0100

    factored out base_token and test to it's own go files.
    The token.go had many types of token types. Also the token_test
    was actually only a base_token test so the test file has been
    renamed appropriately.

---
+++ base_token.go
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2021 IBM Corp and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    https://www.eclipse.org/legal/epl-2.0/
+ * and the Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *    Allan Stockdill-Mander
+ */
+
+package mqtt
+
+import (
+	"sync"
+	"time"
+)
+
+type baseToken struct {
+	m        sync.RWMutex
+	complete chan struct{}
+	err      error
+}
+
+// Wait implements the Token Wait method.
+func (b *baseToken) Wait() bool {
+	<-b.complete
+	return true
+}
+
+// WaitTimeout implements the Token WaitTimeout method.
+func (b *baseToken) WaitTimeout(d time.Duration) bool {
+	timer := time.NewTimer(d)
+	select {
+	case <-b.complete:
+		if !timer.Stop() {
+			<-timer.C
+		}
+		return true
+	case <-timer.C:
+	}
+
+	return false
+}
+
+// Done implements the Token Done method.
+func (b *baseToken) Done() <-chan struct{} {
+	return b.complete
+}
+
+func (b *baseToken) flowComplete() {
+	select {
+	case <-b.complete:
+	default:
+		close(b.complete)
+	}
+}
+
+func (b *baseToken) Error() error {
+	b.m.RLock()
+	defer b.m.RUnlock()
+	return b.err
+}
+
+func (b *baseToken) setError(e error) {
+	b.m.Lock()
+	b.err = e
+	b.flowComplete()
+	b.m.Unlock()
+}
--- token.go
+++ token.go
@@ -17,7 +17,6 @@
 package mqtt
 
 import (
-	"sync"
 	"time"
 
 	"github.com/eclipse/paho.mqtt.golang/packets"
@@ -64,59 +63,6 @@ type tokenCompletor interface {
 	Token
 	TokenErrorSetter
 	flowComplete()
-}
-
-type baseToken struct {
-	m        sync.RWMutex
-	complete chan struct{}
-	err      error
-}
-
-// Wait implements the Token Wait method.
-func (b *baseToken) Wait() bool {
-	<-b.complete
-	return true
-}
-
-// WaitTimeout implements the Token WaitTimeout method.
-func (b *baseToken) WaitTimeout(d time.Duration) bool {
-	timer := time.NewTimer(d)
-	select {
-	case <-b.complete:
-		if !timer.Stop() {
-			<-timer.C
-		}
-		return true
-	case <-timer.C:
-	}
-
-	return false
-}
-
-// Done implements the Token Done method.
-func (b *baseToken) Done() <-chan struct{} {
-	return b.complete
-}
-
-func (b *baseToken) flowComplete() {
-	select {
-	case <-b.complete:
-	default:
-		close(b.complete)
-	}
-}
-
-func (b *baseToken) Error() error {
-	b.m.RLock()
-	defer b.m.RUnlock()
-	return b.err
-}
-
-func (b *baseToken) setError(e error) {
-	b.m.Lock()
-	b.err = e
-	b.flowComplete()
-	b.m.Unlock()
 }
 
 func newToken(tType byte) tokenCompletor {
